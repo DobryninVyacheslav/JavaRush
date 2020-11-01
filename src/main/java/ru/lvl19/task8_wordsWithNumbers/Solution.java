ff449b8a0141153a8ec575b659acd91f941bf878
tester911007 <vladmvm2020@gmail.com>
2020-11-01 18:34:16 +0300

    added solution task 8 (Words with numbers)

---
+++ src/main/java/ru/lvl19/task8_wordsWithNumbers/Solution.java
@@ -0,0 +1,25 @@
+package ru.lvl19.task8_wordsWithNumbers;
+
+import java.io.*;
+
+/*
+Слова с цифрами
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]))) {
+            String[] words;
+
+            while (reader.ready()) {
+                words = reader.readLine().split(" ");
+                for (String word : words) {
+                    if (word.matches(".[0-9].+")) {
+                        writer.write(word + " ");
+                    }
+                }
+            }
+        }
+    }
+}
