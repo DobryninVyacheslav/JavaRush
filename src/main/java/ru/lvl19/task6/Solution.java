03df9656ef2879ce430c4b3730deaa8e01a1e05a
tester911007 <vladmvm2020@gmail.com>
2020-11-01 17:59:21 +0300

    added ready-made solution task 6

--- src/main/java/ru/lvl19/task6/Solution.java
+++ src/main/java/ru/lvl19/task6/Solution.java
@@ -1,37 +1,32 @@
 package ru.lvl19.task6;
 
-import javax.xml.crypto.Data;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
 /*
 Хуан Хуанович
 */
-
 public class Solution {
-    public static final List<Person> PEOPLE = new ArrayList<Person>();
+    public static final List<Person> PEOPLE = new ArrayList<>();
 
-    public static void main(String[] args) {
-//        try (BufferedReader fileReader = new BufferedReader(new FileReader(args[0]))) {
-//            String line;
-//            while ((line = fileReader.readLine()) != null) {
-//                String[] lineValues = line.split(" ");
-//                String name = "";
-//                for (int i = 0; i < lineValues.length - 3; i++) {
-//                    name = name + lineValues[i] + " ";
-//                }
-//                PEOPLE.add(new Person());
-//            }
-            SimpleDateFormat formatter = new SimpleDateFormat("dd MM yyyy");
-//            formatter.parse("");
-//            new Date()
+    public static void main(String[] args) throws IOException, ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd MM yyyy");
 
-//        } catch (IOException e) {
-//
-//        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            String line;
+            while (reader.ready()) {
+                line = reader.readLine();
+
+                PEOPLE.add(new Person(
+                        line.replaceAll("\\d", "").trim(),
+                        format.parse(line.replaceAll("\\D", " ").trim())
+                ));
+            }
+        }
     }
 }
+
